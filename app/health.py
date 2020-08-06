a309489759d54a597753f8ecfbcd5ef74eeffcd7
wesleivarjao <67120657+wesleivarjao@users.noreply.github.com>
2020-08-06 09:56:47 -0300

    Update health.py

--- app/health.py
+++ app/health.py
@@ -41,7 +41,7 @@ def status_fila():
 @app.route('/api')
 def status():
     dict_api = {
-        'api': 1.3, 
+        'api': 1.1, 
         'dep': { 
         'db-sql': status_db(),
         'no-sql': nosql,
